7b992e82b8c9e41a18f66c35919ef6ccc65489f8
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2023-12-13 22:31:31 +0300

    Added fibonacci recursive solution

--- Assignment-1/fibsRecs.js
+++ Assignment-1/fibsRecs.js
@@ -1,3 +1,17 @@
-function fibsRecursive(n) {}
+function fibsRecs(n) {
+  if (n == 1) {
+    return [0];
+  } else if (n == 2) {
+    return [0, 1];
+  } else
+    return [
+      ...fibsRecs(n - 1),
+      fibsRecs(n - 1)[n - 2] + fibsRecs(n - 1)[n - 3],
+    ];
+}
 
-console.log(fibsRecursive(8));
+console.log(fibsRecs(1));
+console.log(fibsRecs(2));
+console.log(fibsRecs(3));
+console.log(fibsRecs(4));
+console.log(fibsRecs(10));
