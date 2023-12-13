ceb3f71fa348c37bc9406c83b9cbb54a674fac92
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2023-12-13 22:33:00 +0300

    fixed a bug with fibs solution

--- Assignment-1/fibs.js
+++ Assignment-1/fibs.js
@@ -2,7 +2,7 @@
 function fibs(n) {
   if (n > 0) {
     const array = [0];
-    for (let i = 1; i < n; ) {
+    for (let i = 1; i < n; i) {
       if (i == 1) {
         array[i] = 1;
         i++;
@@ -16,3 +16,4 @@ function fibs(n) {
 }
 
 console.log(fibs(8));
+console.log(fibs(7));
